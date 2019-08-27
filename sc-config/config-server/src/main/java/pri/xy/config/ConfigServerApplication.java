ae2758d183c5130ee9b9e91e89485f22719b4788
chaohuxiayun <505281348@qq.com>
2019-08-27 14:40:12 +0800

    添加配置中心

---
+++ sc-config/config-server/src/main/java/pri/xy/config/ConfigServerApplication.java
@@ -0,0 +1,23 @@
+package pri.xy.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Classname ServerConfigApplication
+ * @Description
+ * @Date 2019/8/27 9:27
+ * @Author xiayun
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
